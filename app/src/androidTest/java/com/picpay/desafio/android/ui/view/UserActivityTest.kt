c70373c9e6af4dd5005434b9dfeea1be850780d1
Carlos Eduardo Ferreira <duduferreira_01@hotmail.com>
2021-01-19 01:00:01 -0300

    Delete arquivo antigos, add teste instrumental

--- app/src/androidTest/java/com/picpay/desafio/android/ui/view/UserActivityTest.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/ui/view/UserActivityTest.kt
@@ -1,22 +1,21 @@
-package com.picpay.desafio.android
+package com.picpay.desafio.android.ui.view
 
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
+import com.picpay.desafio.android.R
 import com.picpay.desafio.android.feature.user.ui.view.UserActivity
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
 import org.junit.Test
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 
-
-class MainActivityTest {
-
+class UserActivityTest {
     private val server = MockWebServer()
 
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
@@ -28,12 +27,16 @@ class MainActivityTest {
 
             moveToState(Lifecycle.State.RESUMED)
 
-            onView(withText(expectedTitle)).check(matches(isDisplayed()))
+            onView(ViewMatchers.withText(expectedTitle))
+                .check(matches(isDisplayed()))
         }
     }
 
+
     @Test
     fun shouldDisplayListItem() {
+        server.start(serverPort)
+
         server.dispatcher = object : Dispatcher() {
             override fun dispatch(request: RecordedRequest): MockResponse {
                 return when (request.path) {
@@ -43,16 +46,18 @@ class MainActivityTest {
             }
         }
 
-        server.start(serverPort)
-
         launchActivity<UserActivity>().apply {
-            // TODO("validate if list displays items returned by server")
+            server.enqueue(MockResponse().setResponseCode(200))
+            onView(withId(R.id.picture)).check(matches(isDisplayed()))
+            onView(withId(R.id.username)).check(matches(isDisplayed()))
+            onView(withId(R.id.name)).check(matches(isDisplayed()))
         }
 
         server.close()
     }
 
     companion object {
+
         private const val serverPort = 8080
 
         private val successResponse by lazy {
